8a23c0cc50a4d323b6c4e09b30cb190bd0d4e81e
st9540808 <st9540808@gmail.com>
2019-03-03 06:31:01 +0800

    Implement list_merge_sort()
    Along with its helper functions
    get_middle() : return the middle element in a list
    list_merge() : merge two sorted list

--- tests/list_merge_sort.c
+++ tests/list_merge_sort.c
@@ -24,6 +24,64 @@ typedef struct {
 } queue_t;
 
 
+list_ele_t *get_middle(struct list_head *list)
+{
+    struct list_head *fast, *slow;
+    fast = list->next;
+    list_for_each (slow, list) {
+        if (fast->next != list && fast->next->next != list)
+            fast = fast->next->next;
+        else
+            break;
+    }
+    return list_entry(slow, list_ele_t, list);
+}
+
+void list_merge(struct list_head *lhs,
+                struct list_head *rhs,
+                struct list_head *head)
+{
+    struct list_head *temp;
+
+    INIT_LIST_HEAD(head);
+    if (list_empty(lhs)) {
+        list_splice_tail(lhs, head);
+        return;
+    }
+    if (list_empty(rhs)) {
+        list_splice_tail(rhs, head);
+        return;
+    }
+
+    while (!list_empty(lhs) && !list_empty(rhs)) {
+        if (strcmp(list_entry(lhs->next, list_ele_t, list)->value,
+                   list_entry(rhs->next, list_ele_t, list)->value) <= 0)
+            temp = lhs->next;
+        else
+            temp = rhs->next;
+        list_del(temp);
+        list_add_tail(temp, head);
+    }
+    list_splice_tail(list_empty(lhs) ? rhs : lhs, head);
+}
+
+void list_merge_sort(queue_t *q)
+{
+    struct list_head sorted;
+    queue_t left;
+
+    if (list_is_singular(&q->list))
+        return;
+
+    INIT_LIST_HEAD(&left.list);
+    list_cut_position(&left.list, &q->list, &get_middle(&q->list)->list);
+    list_merge_sort(&left);
+    list_merge_sort(q);
+    list_merge(&left.list, &q->list, &sorted);
+    INIT_LIST_HEAD(&q->list);
+    list_splice_tail(&sorted, &q->list);
+}
+
 queue_t *q_new()
 {
     queue_t *q = malloc(sizeof(queue_t));
